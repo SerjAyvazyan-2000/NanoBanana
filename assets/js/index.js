3d532bcba24004d1fada827e51262f3e718d6072
Serzh Ayvazyan <ayvazyanserj3@gmail.com>
2025-12-05 01:42:06 +0400

    Serzh Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,6 @@
+const items = document.querySelectorAll(".a-accordion-item");
 
-  const items = document.querySelectorAll(".a-accordion-item");
-
-items.forEach(item => {
+items.forEach((item) => {
   const header = item.querySelector(".a-accordion-header");
 
   header.addEventListener("click", () => {
@@ -12,16 +11,69 @@ items.forEach(item => {
     }
 
     // Закрыть все остальные
-    items.forEach(i => i.classList.remove("active"));
+    items.forEach((i) => i.classList.remove("active"));
 
     // Открыть текущую
     item.classList.add("active");
   });
 });
 
-// Изначально открыт первый
 items[0].classList.add("active");
 
+$(function () {
+  let Accordion = function (el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+    let links = this.el.find(".link");
+    links.on("click", { el: this.el, multiple: this.multiple }, this.dropdown);
+  };
 
+  Accordion.prototype.dropdown = function (e) {
+    let $el = e.data.el;
+    let $this = $(this),
+      $next = $this.next();
 
-  
+    $next.slideToggle();
+
+    if (!e.data.multiple) {
+      $el.find(".submenu").not($next).slideUp();
+    }
+
+    if (!$this.hasClass("open")) {
+      $(".link").removeClass("open");
+      $this.addClass("open");
+    } else {
+      $this.removeClass("open");
+    }
+  };
+
+  let accordion = new Accordion($("#accordion"), false);
+
+  let firstLink = $("#accordion .link").first();
+  let firstSub = firstLink.next(".submenu");
+
+  firstLink.addClass("open");
+  firstSub.show();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const animatedItems = document.querySelectorAll(
+    ".fade-left, .fade-right, .fade-top, .fade-bottom"
+  );
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+        }
+      });
+    },
+    {
+      threshold: 0.15,
+    }
+  );
+
+  animatedItems.forEach((item) => observer.observe(item));
+});
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,93 +1,91 @@
-
 const detailsSwiper = new Swiper(".detailsSwiper", {
-    slidesPerView: 1, 
-    centeredSlides: true,
-    spaceBetween: 30,
-    loop: true,
-    speed: 600,
-  
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  
-    observer: true,
-    observeParents: true,
-  
-    breakpoints: {
-      576: { 
-        slidesPerView: 1,
-      },
-      768: { 
-        slidesPerView: 1,
-      },
-      992: { 
-        slidesPerView: 1,
-      },
-      1200: { 
-        slidesPerView: "auto",
-      }
-    }
-  });
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  speed: 600,
 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  observer: true,
+  observeParents: true,
+
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 1,
+    },
+    1200: {
+      slidesPerView: "auto",
+    },
+  },
+});
 
 const styleSwiper = new Swiper(".styleSwiper", {
-    slidesPerView: 1, 
-    centeredSlides: true,
-    spaceBetween: 30,
-    loop: true,
-    speed: 600,
-  
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  speed: 600,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  observer: true,
+  observeParents: true,
+
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
     },
-  
-    observer: true,
-    observeParents: true,
-  
-    breakpoints: {
-      576: { 
-        slidesPerView: 1,
-      },
-      768: { 
-        slidesPerView: 1,
-      },
-      992: { 
-        slidesPerView: 1,
-      },
-      1200: { 
-        slidesPerView: "auto",
-      }
-    }
-  });
+    768: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 1,
+    },
+    1200: {
+      slidesPerView: "auto",
+    },
+  },
+});
 const wordsSwiper = new Swiper(".wordsSwiper", {
-    slidesPerView: 1, 
-    centeredSlides: true,
-    spaceBetween: 30,
-    loop: true,
-    speed: 600,
-  
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  speed: 600,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  observer: true,
+  observeParents: true,
+
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
     },
-  
-    observer: true,
-    observeParents: true,
-  
-    breakpoints: {
-      576: { 
-        slidesPerView: 1,
-      },
-      768: { 
-        slidesPerView: 1,
-      },
-      992: { 
-        slidesPerView: 1,
-      },
-      1200: { 
-        slidesPerView: "auto",
-      }
-    }
-  });
+    768: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 1,
+    },
+    1200: {
+      slidesPerView: "auto",
+    },
+  },
+});
